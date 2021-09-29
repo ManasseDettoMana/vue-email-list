b404e8a33a6a0f3737de483481b14749b03e4d4b
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-09-29 19:27:31 +0200

    esercizio finito

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EmailGetter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import EmailGetter from './components/EmailGetter.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmailGetter,
   }
 }
 </script>
---
+++ src/components/EmailGetter.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="email-getter">
+        <ul>
+            <li v-for="(element, index) in emailList" :key="index">{{element}}</li> 
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'EmailGetter',
+    data: function(){
+        return{
+            emailList : []
+        }
+    },
+    mounted(){
+        for(let i=0;i<10;i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (response) => { 
+                    this.emailList.push(response.data.response);
+                })
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
